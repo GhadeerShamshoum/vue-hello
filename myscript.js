ee2243774f914e0aadd2329eea6a38755dccea76
Ghadeer <ghaora@gmail.com>
2022-01-13 13:26:13 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,14 @@
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+
+let app = new Vue({
+    el: '#welcome',
+    data: {
+        nome:"",
+        cognome:"",
+        foto:"https://www.laramind.com/blog/wp-content/uploads/2020/01/vue.png"
+    }
+})
